305734900d5268cc42bacea4457be7762c5df576
Luahr13 <73291538+Luahr13@users.noreply.github.com>
2023-04-11 21:07:26 -0300

    Estrutura

---
+++ stackTins/src/laboratorioDeEstudo/LabDeEstudo.java
@@ -0,0 +1,253 @@
+package laboratorioDeEstudo;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import unitins.ArrayTins;
+import unitins.StackTins;
+
+public class LabDeEstudo {
+
+	static void bubleSorte(int[] array) {
+
+		int aux = 0;
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length - i - 1; j++) {
+				if (array[j] > array[j + 1]) {
+					aux = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = aux;
+				}
+			}
+		}
+		imprimirArray(array);
+	}
+
+	static void imprimirArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i] + " ");
+		}
+		System.out.println();
+	}
+
+	static long calculaTermoRecursivoFibonacci(int posicao) {
+
+		if (posicao == 1 || posicao == 2) {
+			return posicao = 1;
+		}
+		return calculaTermoRecursivoFibonacci(posicao - 1) +
+				calculaTermoRecursivoFibonacci(posicao - 2);
+	}
+
+	static int calculaFatorial(int numero) {
+		int resultado = 1;
+		if (numero == 0 || numero == 1) {
+			return resultado;
+		}
+		while (numero > 1) {
+			resultado = resultado * numero;
+			numero--;
+		}
+		return resultado;
+	}
+
+	static int calculaFatorialRecursivo(int numero) {
+		if (numero == 0 || numero == 1) {
+			return 1;
+		}
+		return numero * calculaFatorialRecursivo(numero - 1);
+	}
+
+	static int calculaPotencia(int base, int exponent) {
+		if (exponent == 0) {
+			return 1;
+		}
+		return base * calculaPotencia(base, exponent - 1);
+
+	}
+
+	public static void mergeSort(int[] arr) {
+		if (arr.length <= 1) {
+			return;
+		}
+
+		int mid = arr.length / 2;
+		int[] left = Arrays.copyOfRange(arr, 0, mid);
+		int[] right = Arrays.copyOfRange(arr, mid, arr.length);
+
+		mergeSort(left);
+		mergeSort(right);
+		merge(arr, left, right);
+	}
+
+	private static void merge(int[] arr, int[] left, int[] right) {
+		int i = 0;
+		int j = 0;
+		int k = 0;
+
+		while (i < left.length && j < right.length) {
+			if (left[i] < right[j]) {
+				arr[k] = left[i];
+				i++;
+			} else {
+				arr[k] = right[j];
+				j++;
+			}
+			k++;
+		}
+
+		while (i < left.length) {
+			arr[k] = left[i];
+			i++;
+			k++;
+		}
+
+		while (j < right.length) {
+			arr[k] = right[j];
+			j++;
+			k++;
+		}
+	}
+
+	public static void mergeSort(String[] arr) {
+		if (arr.length <= 1) {
+			return;
+		}
+
+		int mid = arr.length / 2;
+		String[] left = new String[mid];
+		String[] right = new String[arr.length - mid];
+
+		for (int i = 0; i < mid; i++) {
+			left[i] = arr[i];
+		}
+		for (int i = mid; i < arr.length; i++) {
+			right[i - mid] = arr[i];
+		}
+
+		mergeSort(left);
+		mergeSort(right);
+		merge(arr, left, right);
+	}
+
+	private static void merge(String[] arr, String[] left, String[] right) {
+		int i = 0;
+		int j = 0;
+		int k = 0;
+
+		while (i < left.length && j < right.length) {
+			if (getSobrenome(left[i]).compareTo(getSobrenome(right[j])) < 0) {
+				arr[k] = left[i];
+				i++;
+			} else if (getSobrenome(left[i]).compareTo(getSobrenome(right[j])) > 0) {
+				arr[k] = right[j];
+				j++;
+			} else {
+				if (left[i].compareTo(right[j]) < 0) {
+					arr[k] = left[i];
+					i++;
+				} else {
+					arr[k] = right[j];
+					j++;
+				}
+			}
+			k++;
+		}
+
+		while (i < left.length) {
+			arr[k] = left[i];
+			i++;
+			k++;
+		}
+
+		while (j < right.length) {
+			arr[k] = right[j];
+			j++;
+			k++;
+		}
+	}
+
+	private static String getSobrenome(String s) {
+		String[] parts = s.split(" ");
+		return parts[parts.length - 1];
+	}
+
+	public static void main(String[] args) {
+
+		// int[] arr = { 4, 1, 7, 3, 9, 2 };
+		// mergeSort(arr);
+		// System.out.println(arr);
+
+		// String[] nomes = { "Joao Silva", "Ana Paula Souza", "Ricardo Oliveira", "Camila Rocha", "Felipe Pereira" };
+		// mergeSort(nomes);
+		// for (String nome : nomes) {
+		// 	System.out.println(nome);
+		// }
+
+		/*
+		 * int[] array = {5, 4, 3, 2, 1, 0};
+		 * System.out.println(array);
+		 */
+
+		/*
+		 * int base = 9;
+		 * int exponent = 3;
+		 * System.out.print(base + " elevado a " + exponent + " é: " +
+		 * calculaPotencia(base, exponent));
+		 */
+
+		/*
+		 * int numero = 3;
+		 * System.out.println("Fatorial de " + numero + " é: " +
+		 * calculaFatorial(numero));
+		 * 
+		 * System.out.println("FatorialRecurssivo de " + numero + " é: " +
+		 * calculaFatorialRecursivo(numero));
+		 */
+
+		/*
+		 * int posicao = 6;
+		 * System.out.print("A " + posicao + "° posicao é: " +
+		 * calculaTermoRecursivoFibonacci(posicao));
+		 */
+
+		/*
+		 * try (Scanner ler = new Scanner(System.in)) {
+		 * StackTins<Integer> bin = new StackTins<>();
+		 * int decimal = 0;
+		 * 
+		 * System.out.println("Informe um número em decimal: ");
+		 * decimal = ler.nextInt();
+		 * 
+		 * //Loop
+		 * while (decimal != 0) {
+		 * int resto = decimal % 2;
+		 * bin.push(resto);
+		 * decimal /= 2;
+		 * }
+		 * 
+		 * //Uso o tipo StringBuilder para ter acesso a caracteristicas de stirng
+		 * StringBuilder binario = new StringBuilder();
+		 * while (!bin.isEmpty()) {
+		 * binario.append(bin.pop());
+		 * }
+		 * 
+		 * System.out.println("O número binário é: " + binario.toString());
+		 * }
+		 * 
+		 * StackTins<String> pilha = new StackTins<String>();
+		 * 
+		 * pilha.push("Fusca");
+		 * pilha.push("New Fiesta");
+		 * pilha.push("Corolla");
+		 * pilha.push("Hilux");
+		 * pilha.push("Ferrari");
+		 * pilha.push("Lamborghini");
+		 * 
+		 * while (!pilha.isEmpty()) {
+		 * System.out.println(pilha.pop());
+		 * }
+		 * 
+		 */
+	}
+}
