ea071f5a67e354b0b7ba8bded1b8ca2e73790f52
Luahr13 <73291538+Luahr13@users.noreply.github.com>
2023-04-12 08:22:48 -0300

    ordena string

--- stackTins/src/laboratorioDeEstudo/LabDeEstudo.java
+++ stackTins/src/laboratorioDeEstudo/LabDeEstudo.java
@@ -172,16 +172,73 @@ public class LabDeEstudo {
 		return parts[parts.length - 1];
 	}
 
+	public static void mergeSort(String[] arr, int left, int right) {
+		if (left < right) {
+			int middle = (left + right) / 2;
+			mergeSort(arr, left, middle);
+			mergeSort(arr, middle + 1, right);
+			merge(arr, left, middle, right);
+		}
+	}
+
+	public static void merge(String[] arr, int left, int middle, int right) {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+        
+        String[] L = new String[n1];
+        String[] R = new String[n2];
+        
+        for (int i = 0; i < n1; ++i) {
+            L[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; ++j) {
+            R[j] = arr[middle + 1 + j];
+        }
+        
+        int i = 0, j = 0;
+        int k = left;
+        
+        while (i < n1 && j < n2) {
+            if (L[i].compareTo(R[j]) <= 0) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+	}
+
 	public static void main(String[] args) {
 
+		String[] ay = {"banana", "apple", "cherry", "date", "elderberry", "fig"};
+        System.out.println("ANtes do sorting: " + Arrays.toString(ay));
+        mergeSort(ay, 0, ay.length - 1);
+        System.out.println("Depois sorting: " + Arrays.toString(ay));
+		
+		
 		// int[] arr = { 4, 1, 7, 3, 9, 2 };
 		// mergeSort(arr);
 		// System.out.println(arr);
 
-		// String[] nomes = { "Joao Silva", "Ana Paula Souza", "Ricardo Oliveira", "Camila Rocha", "Felipe Pereira" };
+		// String[] nomes = { "Joao Silva", "Ana Paula Souza", "Ricardo Oliveira",
+		// "Camila Rocha", "Felipe Pereira" };
 		// mergeSort(nomes);
 		// for (String nome : nomes) {
-		// 	System.out.println(nome);
+		// System.out.println(nome);
 		// }
 
 		/*
