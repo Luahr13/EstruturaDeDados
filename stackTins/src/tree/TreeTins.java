6c3be7dec0d397471c028e0ff31cd545cec342b4
Luahr13 <73291538+Luahr13@users.noreply.github.com>
2023-05-03 10:14:13 -0300

    update todday

---
+++ stackTins/src/tree/TreeTins.java
@@ -0,0 +1,39 @@
+package tree;
+
+public class TreeTins {
+    Node rootNode = null;
+
+    public boolean isEmpty(){
+        return rootNode == null;
+    }
+
+    private void addNode(String inf){
+        rootNode = addP(rootNode, inf);
+    }
+
+    private Node addP(Node rootNode, String inf){
+        if (rootNode == null){
+            return new Node(inf);
+        }
+
+        if (inf.compareTo(rootNode.information) < 0){
+            if(rootNode.left == null){
+                rootNode.left = addP(rootNode.left, inf);
+            }else{
+                addP(rootNode.left, inf);
+            }
+        }    
+        else if (inf.compareTo(rootNode.information) > 0){
+            if(rootNode.right == null){
+                rootNode.right = addP(rootNode.right, inf);
+            }else{
+                addP(rootNode.right, inf);
+            }
+        }
+        return rootNode;
+    }
+
+    public void add(String info){
+        rootNode = addP(rootNode, info);
+    }
+}
